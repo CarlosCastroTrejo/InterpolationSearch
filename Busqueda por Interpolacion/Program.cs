d46e66a33017ebcfbe69678dc376de3be000a96e
Carlos Emiliano Castro Trejo <carlos_castro5156@outlook.es>
2019-01-29 20:26:03 -0600

    The algorithm is complete, parameters should be an array of integers and the integer to find. The algorithm returns the position of the number in the array.

---
+++ Busqueda por Interpolacion/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Busqueda_por_Interpolacion
+{
+    class Program
+    {
+
+        static int Interpolacion(int[] arreglo, int numero)
+        {
+            int low = 0;
+            int middle = -1;
+            int high = arreglo.Length - 1;
+            int index = -1;
+
+            while (low <= high)
+            {
+                middle = low + ((high - low) / (arreglo[high] - arreglo[low])) * (numero-arreglo[low]);
+
+                if (arreglo[middle] == numero)
+                {
+                    index = middle;
+                    return index;
+                }
+                else
+                {
+                    if (arreglo[middle] < numero)
+                    {
+                        low = middle + 1;
+                    }
+                    else
+                    {
+                        high = middle - 1;
+                    }
+
+                }
+            }
+
+            return index;
+        }
+        static void Main(string[] args)
+        {
+            int numero = 0,posicion=0;
+            Stopwatch reloj = new Stopwatch();
+
+            int[] arreglo = new int[13] { 1, 3, 10, 14, 22, 32, 44, 58, 69, 101, 115, 133, 244 };
+            Console.WriteLine("Que numero buscas en el arreglo");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            reloj.Start();
+            posicion = Interpolacion(arreglo, numero);
+            reloj.Stop();
+
+            Console.WriteLine("\nEl numero que buscas se encuentra en la posicion: "+posicion);
+            Console.WriteLine("El algoritmo se tardo: " + reloj.Elapsed);
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
